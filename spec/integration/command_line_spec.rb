552f9003310554204ed0f5a19985770911cad34f
Tom Russell <me@tomrussell.co.uk>
2018-07-29 18:07:17 +0100

    Add failing test for basic functionality
    These failing tests describe the basic functionality we want the
    application to have.
    
    At this stage, I'm ignoring the requirement for uniques (I have a
    Plan(tm) for how to build that in later)
    
    This should let me start writing the framework of my app (probably in
    /lib) as classes which I can unit test in isolation.
    
    A single additional error handling case has been implemented here; I
    want the script to verify that the file exists before handing it down to
    the implementing ruby classes; that way I don't have to handle file
    access errors further down the stack.

--- spec/integration/command_line_spec.rb
+++ spec/integration/command_line_spec.rb
@@ -10,6 +10,7 @@ describe "parser.rb" do
 
   context "with no arguments" do
     let(:fixture_path) { "" }
+
     it "returns an error" do
       stdout, stderr, status = Open3.capture3(command)
       expect(status.success?).to eq(false)
@@ -19,13 +20,27 @@ describe "parser.rb" do
     end
   end
 
+  context "with a nonexistent file" do
+    let(:fixture_path) { "spec/fixtures/non_existent_fixture.log" }
+
+    it "returns an error" do
+      stdout, stderr, status = Open3.capture3(command)
+      expect(status.success?).to eq(false)
+      expect(stderr).to match(/file #{fixture_path} does not exist!/)
+    end
+  end
+
   context "with a basic file" do
     # The basic fixture contains three log lines; two for one path and one for another.
     # It was created by snipping a couple of lines from the provided example file.
     let(:fixture_path) { "spec/fixtures/basic.log" }
 
-    it "returns a summary" do
+    it "returns the correct summary of views" do
       stdout, stderr, status = Open3.capture3(command)
+      expect(status.success?).to eq(true)
+      expect(stdout).to match(/\/home 2 visits/)
+      # nb: this singular result we'll want to pluralize or not at some point...
+      expect(stdout).to match(/\/index 1 visits/)
     end
   end
 end
