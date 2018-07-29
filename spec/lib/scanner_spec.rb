20a556d1407f16d157fb23ad06afb4f84ca068ac
Tom Russell <me@tomrussell.co.uk>
2018-07-29 18:42:11 +0100

    Add failing spec for Scanner class
    The desired interface for the Scanner class has been defined in the
    README. Let's write a failing spec for that Scanner to fail until we
    make the interface work.

---
+++ spec/lib/scanner_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+require "scanner"
+
+describe Scanner do
+  subject(:scanner) { described_class.new(string_io) }
+  let(:string_io) { StringIO.new(test_string) }
+
+  context "with a basic fixture" do
+    # Ruby 2.3 introduced squiggly heredocs that strip whitespace. <3 Ruby
+    let(:test_string) {
+      <<~HEREDOC
+      /home 184.123.665.067
+      /home 444.701.448.104
+      /index 444.701.448.104
+      HEREDOC
+    }
+
+    it "yields each line" do
+      expect { |b| scanner.each_line(&b) }.to yield_control.exactly(3).times
+    end
+
+    it "yields the path and IP correctly" do
+      expect { |b| scanner.each_line(&b) }.to yield_successive_args(
+        ["/home", "184.123.665.067"],
+        ["/home", "444.701.448.104"],
+        ["/index", "444.701.448.104"],
+      )
+    end
+  end
+end
