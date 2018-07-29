0c77f654c2182e0ef84ed1bd53091e7cec903b98
Tom Russell <me@tomrussell.co.uk>
2018-07-29 19:15:41 +0100

    Implement #each_by_hits
    This uses a simple sort on the hash to return a sorted array of values.
    
    Pretty sure this isn't as efficient as it could be, but my plan at this
    stage is:
    
      1. Get everything functionally working (and the integration spec
         passing)
      2. Add some benchmarks around parsing the actual file
      3. Improve/speed up the algo

--- lib/in_memory_repository.rb
+++ lib/in_memory_repository.rb
@@ -1,7 +1,15 @@
 class InMemoryRepository
   def store(path, ip)
+    registry[path] ||= { path: path, hits: 0 }
+    registry[path][:hits] += 1
   end
 
   def each_by_hits
+    registry.values.sort { |a, b| b[:hits] <=> a[:hits] }
+  end
+
+  private
+  def registry
+    @registry ||= {}
   end
 end
