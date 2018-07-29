2c9634c448ddd732feed6be30de1ec2701c0b56a
Tom Russell <me@tomrussell.co.uk>
2018-07-29 18:14:27 +0100

    Open an IO object to the provided file
    Error (and exit) if that file doesn't exist.

--- parser.rb
+++ parser.rb
@@ -10,3 +10,18 @@ if ARGV.size < 1
   # break out with an unsuccessful execution
   exit 1
 end
+
+# At this stage, we can assume ARGV[0] contains a path to a file. Let's create
+# a file object pointing to it and bomb out if we can't access that file.
+
+file_path = ARGV[0]
+
+begin
+  file_handle = File.open(file_path) # "r" is the default file open mode
+rescue Errno::ENOENT
+  # This error is raised mostly if the file doesn't exist
+  STDERR.puts "file #{file_path} does not exist!"
+  exit 1
+end
+
+# Now we've got a file handle pointing to a valid file. Let's start some analysis...
