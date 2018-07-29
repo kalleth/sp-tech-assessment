db509183e80568c6520ae639f895ab3997b84d2d
Tom Russell <me@tomrussell.co.uk>
2018-07-29 19:03:19 +0100

    Add failing tests to #each_by_hits
    This spec covers many assertions; the repository is the meat of the
    "implementation" of this technical assessment.
    
    It's going to handle the sorting, indexing, and so on -- but the good
    news is if we implement it naively now we'll be able to refactor later.
    
    To make this test pass, we'll need to implement some form of data
    structure to be then subsequently sorted. Quite a bit of code coming in
    the next commit I think.

---
+++ lib/in_memory_repository.rb
@@ -0,0 +1,7 @@
+class InMemoryRepository
+  def store(path, ip)
+  end
+
+  def each_by_hits
+  end
+end
---
+++ spec/lib/in_memory_repository.rb
@@ -0,0 +1,52 @@
+require "spec_helper"
+require "in_memory_repository"
+
+describe InMemoryRepository do
+  subject(:repo) { InMemoryRepository.new }
+
+  describe "#store" do
+    it "accepts path and IP" do
+      repo.store("/", "1.1.1.1")
+    end
+  end
+
+  describe "#each_by_hits" do
+    context "for a single path" do
+      before do
+        repo.store("/home", "1.1.1.1")
+        repo.store("/home", "1.1.1.1")
+        repo.store("/home", "1.1.1.1")
+      end
+
+      it "returns a nested array of paths and their hits" do
+        expect(repo.each_by_hits).to eq(
+          [
+            ["/home", 3]
+          ]
+        )
+      end
+    end
+
+    context "for multiple paths" do
+      # The IP isn't important here; it can be ignored for hits
+      before do
+        repo.store("/magicians", "1.1.1.1")
+        repo.store("/home", "1.1.1.1")
+        repo.store("/welcome", "1.1.1.1")
+        repo.store("/home", "1.1.1.1")
+        repo.store("/home", "1.1.1.1")
+        repo.store("/welcome", "1.1.1.1")
+      end
+
+      it "returns a sorted, nested array of paths and hits" do
+        expect(repo.each_by_hits).to eq(
+          [
+            ["/home", 3],
+            ["/welcome", 2],
+            ["/magicians", 1],
+          ]
+        )
+      end
+    end
+  end
+end
